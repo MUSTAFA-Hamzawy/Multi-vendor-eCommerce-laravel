f79c15df498a1d55f55ee1bbf8c51275243ae971
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-11 16:10:04 +0200

    DB seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // data
+        $data = [
+            'admin' => [
+                'name' => 'Mustafa Hamzawy',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin1234'),
+                'username' => 'hamzawy1',
+                'role' => 'admin',
+                'status' => 1]
+            ,
+            'vendor' => [
+                'name' => 'Mustafa Vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('vendor1234'),
+                'username' => 'hamzawy2',
+                'role' => 'vendor',
+                'status' => 1]
+            ,
+            'user' => [
+                'name' => 'Mustafa User',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('user1234'),
+                'username' => 'hamzawy3',
+                'role' => 'user',
+                'status' => 1]
+
+        ];
+
+        // seed the db
+        foreach ($data as $role => $userData)
+            DB::table('users')->insert($userData);
+    }
+}
