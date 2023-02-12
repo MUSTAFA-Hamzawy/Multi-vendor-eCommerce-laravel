82d94406c93d3605289f3dbee7f3cc6bb91a37f9
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-12 20:42:50 +0200

    Vendor Controller

---
+++ app/Http/Controllers/User/VendorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Requests\User\VendorInfoRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class VendorController extends UserController
+{
+    /**
+     * To handle the request of updating info of a vendor
+     * @param VendorInfoRequest $request
+     */
+    public function updateInfo(VendorInfoRequest $request){
+        // validation
+        $data = $request->validated();
+
+        // preparing some needed data
+        $userId = Auth::id();
+        $userData = [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'username' => $data['username'],
+            'phone_number' => $data['phone_number'],
+            'address' => $data['address']
+        ];
+
+        $shopData = [
+            'shop_description' => $data['shop_description'],
+            'shop_name' => $data['shop_name']
+        ];
+
+        $vendor_id = DB::table('vendor_shop')
+                    ->where('user_id', '=', $userId)
+                    ->get(['vendor_id'])[0];
+
+        if ($this->updateUserData($userId, $userData) && $this->updateShopData((int)$vendor_id->vendor_id, $shopData))
+            return response(['msg' => "Your Info is updated successfully"], 200);
+        else{
+            toastr()->error('Failed to save changes, try again.');
+            return redirect()->route('vendor-profile');
+        }
+
+    }
+
+    /**
+     * @param int $userId
+     * @param array $data
+     * @return bool
+     */
+    private function updateUserData(int $userId, Array $data): bool{
+        return User::findOrFail($userId)->update($data);
+    }
+
+    /**
+     * @param int $vendor_id
+     * @param array $data
+     * @return bool
+     */
+    private function updateShopData(int $vendor_id, Array $data): bool{
+        return DB::table('vendor_shop')->where('vendor_id', '=', $vendor_id)->update($data);
+    }
+
+}
---
+++ app/Http/Requests/User/VendorInfoRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class VendorInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return 1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+
+        return [
+            'name' => ['required', 'string', 'max:255', 'regex:/^[\pL\s\-]+$/u'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore(Auth::id())],
+            'username' => ['required', 'string', 'max:100', Rule::unique('users')->ignore(Auth::id())],
+            'shop_name' => ['required', 'string', 'max:200', 'regex:/^[\pL\s\-]+$/u',
+                Rule::unique('vendor_shop')->ignore(Auth::id(), 'user_id')],
+            'shop_description' => ['required', 'string'],
+            'phone_number' => ['nullable', 'string', 'max:20'],
+            'address' => ['nullable', 'string', 'max:200'],
+        ];
+    }
+}
