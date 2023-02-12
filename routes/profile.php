dfbabb8cdaff8eec161b6c2a5f03baf497074168
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-12 20:40:33 +0200

    Added new routes

--- routes/profile.php
+++ routes/profile.php
@@ -1,16 +1,28 @@
 <?php
 
+use App\Http\Controllers\User\AdminController;
 use App\Http\Controllers\User\VendorController;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| vendor Routes
+| Profile Routes
 |--------------------------------------------------------------------------
 */
 
+// for admin
+Route::middleware(['auth', 'auth.role:admin'])
+    ->prefix('admin')
+    ->name('admin-')
+    ->controller(AdminController::class)->group(function (){
+        // profile
+        Route::view('profile', 'backend.profile.admin_profile')->name('profile');
+        Route::post('profile/update_info', 'updateInfo')->name('profile-info-update');
+        Route::post('profile/update_image', 'updateImage')->name('profile-image-update');
+        Route::post('profile/update_password', 'updatePassword')->name('profile-password-update');
+    });
+
+// for vendor
 Route::middleware(['auth', 'auth.role:vendor'])
     ->prefix('vendor')
     ->name('vendor-')
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// for normal users
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/vendor.php';
+require __DIR__.'/profile.php';
