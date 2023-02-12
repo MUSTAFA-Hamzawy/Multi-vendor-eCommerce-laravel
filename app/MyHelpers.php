6d5ba7292ef4331a1912f8acd9d63bb370e830f9
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-12 20:41:44 +0200

    Create MyHelpers.php

---
+++ app/MyHelpers.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+
+class MyHelpers
+{
+
+    /**
+     * @param string $originalName
+     * @return string
+     */
+    public static function encryptFileName(string $originalName): string{
+        return substr(md5(time() . $originalName), 0, 80);
+    }
+
+    /**
+     * @param $file
+     * @param string $path
+     * @return string
+     */
+    public static function uploadFile($file, string $path): string{
+        // encrypt the file name
+        $extension = $file->getClientOriginalExtension();
+        $encryptedName = self::encryptFileName($file->getClientOriginalName() . time());
+        $fileName = $encryptedName . '.' . $extension;
+        $file->move($path, $fileName);
+        return $fileName;
+    }
+
+    /**
+     * @param $image
+     * @param string $relativePath
+     * @return string
+     */
+    public static function uploadImage($image, string $relativePath): string{
+        return MyHelpers::uploadFile($image, public_path($relativePath));
+    }
+
+    /**
+     * @param string $imageName
+     * @param string $relativePath
+     * @return void
+     */
+    public static function deleteImageFromStorage(string $imageName, string $relativePath){
+        // delete image from the uploaded images file
+        $image = public_path($relativePath) . $imageName;
+        try {
+            unlink($image);
+        }catch (Exception $exception){
+            // log that exception
+        }
+    }
+
+    /**
+     * @param string $timestamp
+     * @return string
+     */
+    public static function getDiffOfDate(string $timestamp): string{
+        $result = Carbon::parse($timestamp)->diffForHumans();
+        return $result;
+    }
+}
