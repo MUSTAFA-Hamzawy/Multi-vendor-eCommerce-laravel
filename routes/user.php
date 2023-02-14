752dea609ef4a7ab81119deb121142ee24aea2a4
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-14 22:50:49 +0200

    Added the routes of the user

---
+++ routes/user.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+*/
+
+// for normal users
+Route::middleware(['guest'])->group(function (){
+    Route::view('/user_login', 'frontend.auth.login')->name('user-login');
+    Route::view('/user_register', 'frontend.auth.register')->name('user-register');
+});
+
+Route::view('/dashboard', 'frontend.account.account')
+    ->middleware(['auth'])
+    ->name('user-dashboard');
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,18 +17,9 @@ Route::get('/', function () {
     return view('index');
 });
 
-// for normal users
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/vendor.php';
 require __DIR__.'/profile.php';
+require __DIR__.'/user.php';
