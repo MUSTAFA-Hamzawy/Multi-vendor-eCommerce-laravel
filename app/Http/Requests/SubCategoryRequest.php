d8406a6ef6930e63f8f2f3dc9606ed362d4bd574
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-19 23:13:22 +0200

    Validation Request of the sub category

---
+++ app/Http/Requests/SubCategoryRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SubCategoryRequest extends FormRequest
+{
+    private const ALLOWED_EXTENSION = 'jpg,jpeg,png,webp,gif';
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return 1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        // get the brand id ( for updating only )
+        $currentSubCategoryId = 0;
+        if ($this->has('sub_category_id')){
+            $currentSubCategoryId = $this->get('sub_category_id');
+        }
+
+        return [
+            'sub_category_name' => ['required', 'string', 'max:150', Rule::unique('sub_categories')->ignore
+            ($currentSubCategoryId, 'sub_category_id')],
+            'sub_category_image' => [$currentSubCategoryId ? 'nullable' :'required', 'image', 'mimes:' .
+                self::ALLOWED_EXTENSION],
+            'category_id' => ['required', 'int']
+        ];
+    }
+
+    public function messages()
+    {
+        return [        // TODO-me: this is not working ( confirm )
+            'category_id.required' => 'Category is required',
+            'category_id.int' => 'Category is required',
+        ];
+    }
+}
