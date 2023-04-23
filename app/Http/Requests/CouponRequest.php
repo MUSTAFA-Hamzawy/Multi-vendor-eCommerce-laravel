abbd5739eed97480f60b48bec731610369a37394
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-04-23 23:34:09 +0200

    Created the validation request for the coupon

---
+++ app/Http/Requests/CouponRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CouponRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return 1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        // get the coupon id ( for updating only )
+        $currentCouponId = 0;
+        if ($this->has('coupon_id')){
+            $currentCouponId = $this->get('coupon_id');
+        }
+
+        return [
+            'coupon_code' => ['required', 'string', 'max:250', Rule::unique('coupons')->ignore
+            ($currentCouponId, 'coupon_id')],
+            'discount_amount' => ['required', 'numeric', 'gt:0', 'lt:100'],
+            'expiration_date' => ['required', 'date', 'after:today']
+        ];
+    }
+}
