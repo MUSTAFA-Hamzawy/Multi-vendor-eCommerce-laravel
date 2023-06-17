d84bb733499c9f7969a4868569d3f7cebb665c8d
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-06-17 21:24:33 +0200

    Updated some logic of the coupon
    The vendor can show only his created coupon
    The admin can show all coupons

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\User\VendorController;
 use App\Http\Requests\CouponRequest;
 use App\Models\CouponModel;
+use App\Models\User;
+use App\Notifications\CouponInsertedNotification;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 
 class CouponController extends Controller
 {
+    // TODO: event of the coupon on the database table is not working
+    public function getAllCoupons(){
+        if (Auth::user()->role == 'admin')
+            $data = CouponModel::all();
+        else
+            $data = CouponModel::where('vendorId', VendorController::getVendorId(Auth::id()))->get();
+
+        return view('backend.coupon.coupon_default', compact('data'));
+    }
+
     /**
      * @param CouponRequest $request
      * @return Response
@@ -18,9 +33,17 @@ class CouponController extends Controller
         // validation
         $data = $request->validated();
 
+        $data['VendorId'] = VendorController::getVendorId(Auth::id());
+
         // insertion
-        if (CouponModel::insert($data))
+        if (CouponModel::insert($data)){
+            // notify the admins
+            $admins = User::where('role', 'admin')->get();
+            $shopName = DB::table('vendor_shop')->where('user_id', Auth::id())->get('shop_name')[0]->shop_name;
+            Notification::send($admins, new CouponInsertedNotification($shopName));
             return response(['msg' => 'Coupon is created successfully.'], 200);
+        }
+
         else
             return redirect('coupons')->with('error', 'Failed to add this coupon, try again.');
 
