02ea9704c6430e40f617d738c9ae73b95aa3f40f
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-06-17 21:10:44 +0200

    updated some routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::fallback(function (){
+    return redirect()->route('login');
+});
+
 
 require_once __DIR__.'/auth.php';
 require_once __DIR__.'/admin.php';
@@ -29,4 +33,5 @@ require_once __DIR__.'/sub_category.php';
 require_once __DIR__.'/product.php';
 require_once __DIR__.'/coupon.php';
 require_once __DIR__.'/notifications.php';
+require_once __DIR__.'/socialite.php';
 
